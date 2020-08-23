558d39b277903591b54f75f4383b2b052c8ce978
JessikaSuniga <jessikasuniga@gmail.com>
2020-08-23 20:44:53 -0300

    JS adicionado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-new SimpleSlide({
-    slide: 'principal', // nome do atributo data-slide="principal"
-    nav: true, // se deve ou não mostrar a navegação
-    auto: true, // se o slide deve passar automaticamente
-    time: 5000, // tempo de transição dos slides
-    pauseOnHover: true, // pausa a transição automática
-  });
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+}
